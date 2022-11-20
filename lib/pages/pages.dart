2c3ac7bf22a8dd7bcd1395548118ca02a92df10e
David <davidarrozaqi085@gmail.com>
2022-11-20 19:27:36 +0700

    FIrst

---
+++ lib/pages/pages.dart
@@ -0,0 +1,13 @@
+// import 'dart:ffi';
+// import 'dart:html';
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:mop_green/shared/shared.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+import 'package:syncfusion_flutter_charts/charts.dart' as chart;
+import 'dart:math' as math;
+
+part 'selamatdatang_pages.dart';
+part 'dashboard.dart';
